7cf507a497c80064e96fd337351b93f64f2e1342
Lucas Leonardo <lucasleonardo@protonmail.com>
2024-02-19 11:32:13 -0300

    misc: add logic for checkboxes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from "react";
+import "./App.css";
+
+interface Label {
+  label: string;
+  value: boolean;
+}
+
+type ActionToggleAll = {
+  type: "toggleAll";
+};
+
+type ActionToggleOne = {
+  type: "toggleOne";
+  payload: {
+    index: number;
+  };
+};
+
+type LabelAction = ActionToggleAll | ActionToggleOne;
+
+function reducer(state: Label[], action: LabelAction): Label[] {
+  switch (action.type) {
+    case "toggleAll":
+      state.forEach((l) => (l.value = !l.value));
+      return [...state];
+    case "toggleOne":
+      state[action.payload.index].value = !state[action.payload.index].value;
+      return [...state];
+    default:
+      return state;
+  }
+}
+
+const countries: Label[] = [
+  { label: "India", value: false },
+  { label: "USA", value: false },
+  { label: "France", value: false },
+];
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, countries);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        <li>
+          <label>
+            <input
+              type="checkbox"
+              onChange={(e) =>
+                dispatch({
+                  type: "toggleAll",
+                })
+              }
+            />
+            Select all
+          </label>
+        </li>
+        {state.map((l, i) => (
+          <li>
+            <label>
+              <input
+                type="checkbox"
+                checked={l.value}
+                onChange={(_) =>
+                  dispatch({
+                    type: "toggleOne",
+                    payload: { index: i },
+                  })
+                }
+              />
+              {l.label}
+            </label>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
